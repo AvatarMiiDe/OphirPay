159391e460ccf0716ead15b48edd3cc48bfbd5ec
CodingAngel1 <pappyviews@gmail.com>
2026-08-05 09:01:32 +0000

    refactor: add barrel export for UI components (src/components/ui/index.ts)

---
+++ src/components/ui/index.ts
@@ -0,0 +1,17 @@
+export { Button } from "./Button";
+export type { ButtonVariant, ButtonSize } from "./Button";
+export { Card } from "./Card";
+export { Modal } from "./Modal";
+export { ToastProvider, useToast } from "./Toast";
+export { Spinner } from "./Spinner";
+export { Badge, StatusBadge } from "./Badge";
+export { Input, Textarea, Select } from "./Form";
+export { CopyButton } from "./CopyButton";
+export { ExplorerLink } from "./ExplorerLink";
+export { ConfirmDialog } from "./ConfirmDialog";
+export { Tooltip } from "./Tooltip";
+export { Pagination } from "./Pagination";
+export { ProgressBar } from "./ProgressBar";
+export { Skeleton } from "./Skeleton";
+export { Kbd } from "./Kbd";
+export { Amount } from "./Amount";
